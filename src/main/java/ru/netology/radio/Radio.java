85642bbc674eb3ae4a3d79eb67c4ea62b3f19634
OEM <gurleva_ludmila@mail.ru>
2023-07-24 23:44:39 +0300

    Cut back code

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 
+
 public class Radio {
     private int maxStation = 9;
     private int minStation = 0;
@@ -15,7 +16,7 @@ public class Radio {
     private int maxVolume = 100;
     private int minVolume = 0;
     private int currentVolume;
-    
+
 }
 
 
